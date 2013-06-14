7b2e4400f805f98951cad76c7419dcbaea62674a
John Fawcett <jrf0110@gmail.com>
2013-06-14 11:55:21 -0500

    initial

---
+++ app.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', 3010);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+// app.use(function(req, res, next){
+//   if (req.header['authorization'] != process.env.GB_DEPLOY_ALERT_AUTH)
+//     return req.status(401).json({ name: "NOT_AUTHENTICATED" });
+
+//   next();
+// });
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var deployments = [];
+
+app.post('/deployments', function(req, res){
+  deployments.push(req.body.app);
+});
+
+app.get('/deployments', function(req, res){
+  res.json({ app: deployments.pop() });
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
